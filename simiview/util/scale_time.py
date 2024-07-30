3449178dcee49c3a04130bd0775efac56d1af003
Janahan Selvanayagam <jselvan5@uwo.ca>
2024-07-30 10:57:17 +0100

    add util fun scale time

---
+++ simiview/util/scale_time.py
@@ -0,0 +1,11 @@
+scale = {
+    'hr': 60**2,
+    'min': 60,
+    's': 1,
+    'ms': 1e-3,
+    'us': 1e-6
+}
+def get_scale_factor(time, input_units, output_units, sampling_rate=None):
+    input_scale = sampling_rate if input_units=='sampling_rate' else scale[input_units]
+    output_scale = sampling_rate if output_units=='sampling_rate' else scale[output_units]
+    return time / input_scale * output_scale
